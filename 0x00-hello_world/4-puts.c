9365118140f8807b2b6481ac4e0c8f0b6c94c30b
sara <userofgits@gmail.com>
2023-06-15 20:25:20 -0700

    corrected tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
