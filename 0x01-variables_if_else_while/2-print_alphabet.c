bd000b06b393df49742468687a9f46f76c01e078
sara <userofgits@gmail.com>
2023-06-16 18:17:07 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *Program that prints the alphabet
+ * Return: 0 on success
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{putchar(alpha);
+putchar("\n");
+alpha++; }
+return (0);
+}
