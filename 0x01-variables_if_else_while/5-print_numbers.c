9a672e6cfddcdfb6175eef678156743d0c93eec9
sara <userofgits@gmail.com>
2023-06-16 18:53:28 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *program that prints single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf("%d", n);
+}
+return (0);
+}
