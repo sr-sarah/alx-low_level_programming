252a66f96558adc36035805d20291e9f1b740369
sara <userofgits@gmail.com>
2023-06-23 18:06:51 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9.
+ * Description: printing the numbers from 0 to 9.
+ * Return: 0 on success.
+ */
+
+void print_numbers(void)
+{
+for (i = 48; i <= 57; i++)
+{
+_putchar ("i");
+}
+_putchar ("\n");
+}
