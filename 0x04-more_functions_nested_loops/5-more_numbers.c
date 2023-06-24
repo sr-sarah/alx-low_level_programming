812cd4614102b68fe8b22b29b8ed2436ac1499b5
sara <userofgits@gmail.com>
2023-06-23 19:25:17 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,14 @@ void more_numbers(void)
 {
 int i;
 int j;
-int k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-k = j % 10;
 if (j > 9)
-{_putchar('1');}
-_putchar(k + '0');
+{
+_putchar((j / 10) + '0');}
+_putchar((j % 10) + '0');
 }
 _putchar ('\n');
 }
