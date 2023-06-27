2a099c444f9622258449c870f68e1761adb34b9c
sara <userofgits@gmail.com>
2023-06-26 19:53:41 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string.
+ * @s: the parameter
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+int len;
+int i;
+char c;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+for (i = 0; i < len / 2; i++)
+{
+c = s[i];
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = c;
+}
+}
