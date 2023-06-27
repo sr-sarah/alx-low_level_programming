95ab0c5e99b622e755e0913e402a2dc29c0547e5
sara <userofgits@gmail.com>
2023-06-26 20:02:02 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - returns the length of a string.
+ * @str: the parameter
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+int j = 0;
+char *c = str;
+int k;
+
+while (*c != '\0')
+{
+c++;
+i++;
+}
+
+j = i - 1;
+for (k = 0; k <= j; k++)
+{
+if (k % 2 == 0)
+{
+_putchar(str[k]);
+}
+_putchar('\n');
+}
