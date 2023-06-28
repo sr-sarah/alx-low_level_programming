a527f9621d322cc6166b30bf539784f677a65887
sara <userofgits@gmail.com>
2023-06-27 18:51:21 -0700

    task 7!

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,18 @@ void puts_half(char *str)
 {
 int i;
 int len = 0;
-int n = len / 2;
 
 for (i = 0; str[i] != '\0'; i++)
 {
 len++;
 }
 
-if ((len % 2) == 1)
+if ((len % 2) == 0)
+{
+n = len / 2;
+}
+
+else if ((len % 2) == 1)
 {
 n = ((len + 1) / 2);
 }
